919d74649bd89a81bbb43fe0b9b594818a95dbec
bertao01 <victorbertaomonteiro@gmail.com>
2021-05-26 18:04:35 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
@@ -9,7 +8,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
 
@@ -22,7 +20,7 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
-          <Route path="/detail">
+          <Route path="/detail/:id">
             <Detail />
           </Route>
           <Route path="/">
